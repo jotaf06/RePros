ac7b3d3cd54fe6cd0a8cd312d13d329c26d72873
viniciusCalcantara <vclga@ic.ufal.br>
2023-07-17 00:01:27 -0300

    criação de grupos e adição de membros

--- group_operation.py
+++ group_operation.py
@@ -1,22 +1,67 @@
 from load_functions import salva_os_grupos
 
 def criar_grupo(users, user_login, groups):
-    """
-    Cria um grupo
-    O usuário 0 é o administrador
-    """
+    """Cria um grupo. O usuário 0 é o administrador"""
     while True:
         new_group_name = input("\nNome do grupo: ")
         if new_group_name in groups:
             print("Ja existe um grupo com esse nome.")
         else:
             user = users[user_login]
-            groups[new_group_name] = [user['nickname']]
+            groups[new_group_name] = [[user['nickname'], 'admin']]
             salva_os_grupos(groups)
             print(f"\nGrupo criado com sucesso {user['nickname']}.")
             break
 
+def add_membro(user_membro, groups, group):
+    """Adiciona um membro"""
+    if not (user_membro[1] == 'admin' or user_membro[1] == 'sub_admin'):
+        print("Você não tem permissão para realizar essa operação...")
 
+    elif user_membro[1] == 'admin':
+        print("Olá admin")
+        new_membro_nickname = input("Quem você deseja adicionar no seu grupo?: ")
+        cargo = input("Qual o nível de acesso do usuário?: ")
+
+        group.append([new_membro_nickname, cargo])
+        salva_os_grupos(groups)
+        print("Operação bem sucedida.")
+
+def eh_membro(user_nickname, group):
+    """Verifica se um usuario eh membro de certo grupo"""
+    for user in group:
+        if user[0] == user_nickname:
+            return user
+    return None
+
+def entrar_grupo(users, user_login, groups):
+    """Aqui acontece a interação do usuario com o grupo"""
+    group_name = input("\nGrupo que se deseja entrar: ")
+
+    if group_name not in groups:
+        print("Esse grupo não existe.")
+    else:
+        group = groups[group_name]
+        user_nickname = users[user_login]['nickname']
+        print(user_nickname)
+        user_membro = eh_membro(user_nickname, group)
+        if user_membro != None:
+            print(f"\nBem vindo a {group_name}, {user_nickname}.")
+            
+            while True:
+                print("\n'add_membro' - para adicionar membro.")
+                print("'sair' - para sair do grupo.")
+                
+                command = input("comando: ")
+                if command == 'add_membro':
+                    add_membro(user_membro, groups, group)
+
+                elif command == 'sair':
+                    print("Saindo do grupo...")
+                    break
+        
+        else:
+            print("Você não é membro desse grupo!")
 
 
 
--- singning_in.py
+++ singning_in.py
@@ -1,11 +1,11 @@
 from editar_usuario import edit_user
 from acesso_a_perfil import show_perfil
-from group_operation import criar_grupo
+from group_operation import criar_grupo, entrar_grupo
 
-def entrando_na_rede_repros(users, user, groups):
+def entrando_na_rede_repros(users, user_login, groups):
     """Aqui ocorre toda a interação do usuário com a rede"""
     
-    print(f"Olá {users[user]['nickname']} Você está conectado a rede.")
+    print(f"Olá {users[user_login]['nickname']} Você está conectado a rede.")
      
     sair_da_rede = False
     while sair_da_rede == False:
@@ -17,17 +17,21 @@ def entrando_na_rede_repros(users, user, groups):
             print("'acessar_perfil' - para acessar as informações/arquivos de um usuário.")
             print("'mensagem' - enviar uma mensagem a outro usuario.")
             print("'criar_grupo' - cria um grupo.")
+            print("'entrar_grupo' - entrar em um grupo.")
             print("'adicionar_midia' - adiciona arquivos em seu perfil.")  
             print("'sair' - para se desconectar.\n")  
         
         elif command == 'editar_usuario':
-            edit_user(users, user)
+            edit_user(users, user_login)
 
         elif command == 'acessar_perfil':
             show_perfil(users)
 
         elif command == 'criar_grupo':
-            criar_grupo(users, user, groups)
+            criar_grupo(users, user_login, groups)
+
+        elif command == 'entrar_grupo':
+            entrar_grupo(users, user_login, groups)
 
         elif command == 'sair':
             sair_da_rede = True
