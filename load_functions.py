98c555c8c6a839be17e5d9b6323d2979e63b0aeb
viniciusCalcantara <vclga@ic.ufal.br>
2023-07-17 22:12:56 -0300

    agora carrega/salva as relações de amizade

--- load_functions.py
+++ load_functions.py
@@ -14,11 +14,6 @@ def salva_os_usuarios(users):
     with open('users.json', 'w') as file:
         json.dump(users, file, indent='\t')
 
-def salva_os_grupos(groups):
-    """Salva os grupos em arquivo JSON"""
-    with open('groups.json', 'w') as file:
-        json.dump(groups, file, indent='\t')
-
 def carrega_os_grupos():
     """Carrega os grupos"""
     try:
@@ -27,3 +22,21 @@ def carrega_os_grupos():
             return json.load(file)
     except (FileNotFoundError, json.decoder.JSONDecodeError):
         return {}
+    
+def salva_os_grupos(groups):
+    """Salva os grupos em arquivo JSON"""
+    with open('groups.json', 'w') as file:
+        json.dump(groups, file, indent='\t')
+
+def carrega_amizades():
+    """Carrega as amizades"""
+    try:
+        with open('amigos.json', 'r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        return {}
+
+def salva_amizades(relationship):
+    """Salva as amizades"""
+    with open('amigos.json', 'w') as file:
+        json.dump(relationship, file, indent='\t')
